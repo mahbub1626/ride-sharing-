d3db7d89c32e43f72f94c5d6c024f71092d79946
mahbub1626 <mahbub1626@gmail.com>
2022-12-27 19:05:31 +0600

    about section polishing done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,43 +12,40 @@ const About = () => {
             id: 1,
             title: "Bus Pickup Request, and Track Your Bus",
             img: BusPickupRequest,
-            ca: "row-span-2",
-            p: "lg:pt-32"
+            ca: "row-span-2"
         },
         {
             id: 2,
             title: "Inter-District Bus E-Ticketing Service",
             img: InterDistrictBus,
-            ca: "",
-            p: "py-0"
+            ca: ""
         },
         {
             id: 3,
             title: "Ride Sharing and Rental Service ",
             img: RideSharing,
-            ca: "row-span-2",
-            p: "py-8"
+            ca: "row-span-2"
         },
         {
             id: 4,
             title: "Bus Pickup Request, and Track Your Bus",
             img: BusPickupRequest,
-            ca: "",
-            p: "py-0"
+            ca: ""
         },
 
     ]
     return (
         <div className=''>
-            <div className='grid lg:grid-cols-2'>
+            <div className='flex flex-col-reverse lg:grid lg:grid-cols-2'>
                 <div className='lg:pt-52'>
-                    <img className='busShape lg:grid hidden lg:w-[950px] absolute' src={BusShape} alt="" />
-                    <div className='relative lg:w-[800px] lg:h-[400px] grid lg:grid-cols-3 gap-4 justify-items-start ml-2 py-32'>
+                    <img className='busShape lg:grid hidden lg:w-[981px] absolute' src={BusShape} alt="" />
+                    <div className='relative lg:w-[800px] lg:h-[400px] grid lg:grid-cols-3 gap-4 justify-items-center items-center ml-2 py-28'>
                         {/* dynamic card */}
                         {
                             data.map(d => <div
                                 key={d.id}
-                                className={`grid-flow-col bg-white p-4 ${d.ca} ${d.p} text-center rounded-xl drop-shadow-xl lg:w-[232px] max-w-[232px] lg:h-[254px] mx-auto`}
+                                className={`grid-flow-col bg-white p-4 ${d.ca}
+                                 text-center rounded-xl drop-shadow-xl lg:w-[232px] max-w-[232px] lg:h-[254px] mx-auto`}
                                 >
                                     <img src={d.img} alt="" />
                                     <h2 className='lg:text-xl font-bold py-4'>{d.title}</h2>
@@ -58,11 +55,11 @@ const About = () => {
 
                     </div>
                 </div>
-                <div className='bus lg:py-20 lg:pt-60 pl-4'>
+                <div className='bus lg:py-20 lg:pt-60 pl-4 '>
                     <h2 className='lg:ml-32'>Why we are for you</h2>
                     <p className='lg:ml-32 my-4 lg:w-[596px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five </p>
 
-                    <img src={Bus} alt="Bus" className='' />
+                    <img src={Bus} alt="Bus" className=''/>
 
                 </div>
             </div>
